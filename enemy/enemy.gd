2e8d2367965a1e257c986db493f39a9c083773c1
zzz-ning <wongzline@gmail.com>
2025-09-27 15:33:43 +0800

    filing

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,9 +1,9 @@
-extends ColorRect
+extends Area2D
 
-var speed = 300
+var SPEED = 300
 
 func _process(delta):
-	position.x -= speed * delta
+	position.x -= SPEED * delta
 
 	if position.x < -50:  # off-screen
 		queue_free()
