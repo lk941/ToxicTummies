9c48dc0fe702a7920cea61b7ecb1cbafd1ee7eb5
lk941 <leekee941@gmail.com>
2025-09-28 02:08:51 +0800

    added fonts, start game, score

--- main.gd
+++ main.gd
@@ -1,7 +1,37 @@
 extends Control
 
+@onready var score_label = $ScoreCount
+@onready var timer = $Timer
+
+@onready var play_button = $GameState/StartButton
+@onready var start_label = $GameState/GameStartLabel
+
+var score = 0
+
+func _ready():
+	pass
+
+
+func game_start():
+	score_label.text = str(score)
+	timer.start()
+	timer.timeout.connect(_on_timer_timeout)
+	timer.start()
+
 # Game Over
 #func temp_game_over_func():
 	#%GameOverScreen.visible = true
 	#get_tree().paused = true #pause the game
 	#
+
+
+func _on_timer_timeout():
+	score += 1
+	score_label.text = str(score)
+
+
+func _on_start_button_pressed() -> void:
+	start_label.visible = false
+	play_button.visible = false
+	
+	game_start()
--- player/player.gd
+++ player/player.gd
@@ -16,9 +16,15 @@ func _physics_process(delta):
 	%HungerBar.value = hunger_level
 	%ToxicBar.value = toxic_level
 	
+	if Input.is_action_just_pressed("jump"):
+		jump()
+	
 	#if hunger_level <= 0.0:
 		#hunger_depleted.emit()
 					  
+					
+func jump():
+	anim.play("jump")  
 
 func get_poisoned():
 	%ToxicBar.value += 20
@@ -32,3 +38,8 @@ func get_energized():
 	%HungerBar.value += 10
 	print("energized + 10")
 	anim.play("bagged")    
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $AnimatedSprite2D.animation == "jump":
+		$AnimatedSprite2D.play("idle")
