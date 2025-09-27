e77151829a53f7e7812451bfc27fe0f23f8fd641
zzz-ning <wongzline@gmail.com>
2025-09-27 23:56:10 +0800

    added toxicity and satiety bar

--- player/player.gd
+++ player/player.gd
@@ -5,16 +5,17 @@ var level_of_toxicity = 30
 @onready var anim = $AnimatedSprite2D
 
 func _ready():
-	print(level_of_toxicity)
+	#print(level_of_toxicity)
 	#anim.animation = "idle"
 	anim.play("idle")  
 					  
 
 func get_poisoned():
-	level_of_toxicity += 20
+	%ToxicBar.value += 20
+	#level_of_toxicity += 20
 	anim.play("poisoned")    
 	print(level_of_toxicity)
-	#
-#func get_bagged():
-	#anim.play("bagged")    
 	
+func get_bagged():
+	anim.play("bagged")    
+	print("hi")
--- shield.gd
+++ shield.gd
@@ -8,7 +8,6 @@ func _process(delta):
 	if position.x < -50:  # off-screen
 		queue_free()
 
-
 func _on_body_entered(body):
 	queue_free() 
 	if body.has_method("get_bagged"):
