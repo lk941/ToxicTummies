78327c9efc3a54e8761fac93d4dbbb5b6979bc5d
Xenofields <114409430+ProgrammingTohster@users.noreply.github.com>
2025-09-29 02:02:34 +0800

    re-connect body_entered for shield.tscn

--- shield.gd
+++ shield.gd
@@ -5,18 +5,14 @@ var SPEED = 60
 func _process(delta):
 	if Main.game_started:
 		position.x -= SPEED * delta
-
 		#if position.x < -50:  # off-screen
 			#queue_free()
 
 
-#func _on_body_entered_shield(body: Node2D) -> void:
-	#queue_free() 
-	#if body.has_method("get_bagged"):
-		#body.get_bagged()
 
 
 func _on_body_entered(body):
 	queue_free() 
+	print("Bagged")
 	if body.has_method("get_bagged"):
 		body.get_bagged()
